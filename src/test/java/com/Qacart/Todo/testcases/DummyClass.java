11028f611e5b4c914e4da00b3ad555438048e174
HamdyIbrahimmoh <45590574+HamdyIbrahimmoh@users.noreply.github.com>
2024-06-06 17:08:38 +0300

    tested

--- src/test/java/com/Qacart/Todo/testcases/DummyClass.java
+++ src/test/java/com/Qacart/Todo/testcases/DummyClass.java
@@ -2,20 +2,31 @@ package com.Qacart.Todo.testcases;
 
 import com.Qacart.Todo.api.RegisterAPI;
 import com.Qacart.Todo.objects.User;
+import com.Qacart.Todo.utils.ConfigUtils;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import io.restassured.RestAssured.*;
 import io.restassured.matcher.RestAssuredMatchers.*;
 import io.restassured.response.Response;
 import org.hamcrest.Matchers.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import static io.restassured.RestAssured.given;
 
 public class DummyClass {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-       RegisterAPI registerAPI = new RegisterAPI();
-       registerAPI.register();
-        System.out.println(registerAPI.getAccessToken());
-        System.out.println(registerAPI.getFirstName());
-        System.out.println(registerAPI.getUserId());
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get(ConfigUtils.getInstance().getBaseUrl());
+        driver.manage().window().maximize();
+        Thread.sleep(10000);
+        driver.quit();
+
+//       RegisterAPI registerAPI = new RegisterAPI();
+//       registerAPI.register();
+//        System.out.println(registerAPI.getAccessToken());
+//        System.out.println(registerAPI.getFirstName());
+//        System.out.println(registerAPI.getUserId());
     }
 }
---
+++ src/test/java/com/Qacart/Todo/utils/CookieUtils.java
@@ -0,0 +1,11 @@
+package com.Qacart.Todo.utils;
+
+import io.restassured.http.Cookies;
+
+import java.util.List;
+
+public class CookieUtils {
+    public void convertRestAssuredCookiesToSeleniumCookies(List<Cookies> restAssuredCookie){
+
+    }
+}
